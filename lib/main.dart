eb4bd7476a2c909b38b7a0d0957fc82461bac44f
KaitoSuzuki <>
2021-02-15 02:51:37 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@ import 'package:color_filter_demo/color_selector_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_image_picker/flutter_web_image_picker.dart';
 
-
 const List<Color> _colorList = [
-Color(0xFF414141),
+  Color(0xFF414141),
   Color(0xFF746459),
   Color(0xFF7a5c34),
   Color(0xFFceb56d),
@@ -39,6 +38,7 @@ class ImagePickerPage extends StatefulWidget {
 class _ImagePickerPageState extends State<ImagePickerPage> {
   Image image;
   Color _color = Color(0xFF414141);
+
   @override
   Widget build(BuildContext context) {
     final size = (MediaQuery.of(context).size.height * 0.30).roundToDouble();
@@ -59,18 +59,26 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
       body: Column(
         children: [
           SizedBox(
-              height: size,
-              child: image != null ? ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                _color,
-                BlendMode.modulate,
-              ),
-              child: image) : Center(child: Text('No data...'))),
-          Expanded(child: SingleChildScrollView(child: ColorSelectorField(
+            height: size,
+            child: image != null
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                      _color,
+                      BlendMode.modulate,
+                    ),
+                    child: image)
+                : Center(
+                    child: Text(
+                      'No data...',
+                    ),
+                  ),
+          ),
+          Expanded(
+              child: SingleChildScrollView(
+                  child: ColorSelectorField(
             color: _color,
             colorList: _colorList,
             colorChange: _onColorChange,
-
           )))
         ],
       ),
